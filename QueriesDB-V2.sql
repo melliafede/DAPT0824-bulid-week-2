af484778a09afb2b5633ba5202585c3dab5d1a4c
mellifed <melliafede@gmail.com>
2025-02-26 10:11:46 +0100

    Aggiunta query di aggiornamento con solo id vendita

--- QueriesDB-V2.sql
+++ QueriesDB-V2.sql
@@ -34,18 +34,35 @@ SET Stock = Stock - (SELECT Quantity FROM Sales WHERE Sales.ID = 13 AND Sales.Pr
 WHERE ProductID IN 
 (SELECT ProductID FROM Sales WHERE Sales.ID = 13) AND WarehouseID = (SELECT WarehouseID	FROM Stores WHERE ID = 1);
 
--- Seconda vendita
-INSERT INTO Sales (ID, StoreID, LineID, ProductID, Quantity) VALUES
-(14, 3, 1, 4, 10),
-(14, 3, 2, 10, 5),
-(15, 2, 1, 3, 2);
+ -- Aggiornamento con una sola query per mezzo di un id vendita
+UPDATE StockLevels
+SET Stock = Stock - (SELECT Quantity FROM Sales WHERE Sales.ID = 13 AND Sales.ProductID = StockLevels.ProductID)
+WHERE ProductID IN (SELECT ProductID FROM Sales WHERE Sales.ID = 13) 
+AND WarehouseID = (	SELECT Warehouses.ID FROM Warehouses 
+					JOIN Stores ON Warehouses.ID = Stores.WarehouseID
+					JOIN Sales ON Stores.ID = Sales.StoreID
+					WHERE Sales.ID = 13 LIMIT 1);
 
- -- Aggiornamento con una sola query per mezzo di un id vendita e di un store id
+-- Numero vendita 14 nello store 5
+-- Che prodotti ci sono nello store 5?
+SELECT StockLevels.ProductID 
+FROM StockLevels JOIN Warehouses ON StockLevels.WarehouseID = Warehouses.ID
+JOIN Stores ON Stores.WarehouseID = Warehouses.ID
+WHERE Stores.ID = 5;
+
+-- Vengono venduti i prodotti 4 e 6
+INSERT INTO Sales (ID, StoreID, LineID, ProductID, Quantity) VALUES
+(14, 5, 1, 4, 10),
+(14, 5, 2, 6, 5);
+
+ -- Aggiornamento dello stock con una sola query per mezzo dell'id vendita 14
 UPDATE StockLevels
 SET Stock = Stock - (SELECT Quantity FROM Sales WHERE Sales.ID = 14 AND Sales.ProductID = StockLevels.ProductID)
-WHERE ProductID IN 
-(SELECT ProductID FROM Sales WHERE Sales.ID = 14) AND WarehouseID = (SELECT WarehouseID FROM Stores WHERE ID = 3);
-
+WHERE ProductID IN (SELECT ProductID FROM Sales WHERE Sales.ID = 14) 
+AND WarehouseID = (	SELECT Warehouses.ID FROM Warehouses 
+					JOIN Stores ON Warehouses.ID = Stores.WarehouseID
+					JOIN Sales ON Stores.ID = Sales.StoreID
+					WHERE Sales.ID = 14 LIMIT 1);
 
 
 
